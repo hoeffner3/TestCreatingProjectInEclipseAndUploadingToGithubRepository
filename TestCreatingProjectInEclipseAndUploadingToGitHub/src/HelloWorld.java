006276c535214bd52e784ac3f7e7b85765d5d39d
Steve <hoeffner3@gmail.com>
2015-02-27 17:01:00 -0500

    I have followed the directions in the following youtube video:
    https://www.youtube.com/watch?v=U1kXlahkwd4
    
    I don't know whether 'egit' or 'eclipse github integration with task
    focused interface' helped me achieve this task.

---
+++ TestCreatingProjectInEclipseAndUploadingToGitHub/src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World, this is an eclipse project which I will export to github");
+    }
+
+}
